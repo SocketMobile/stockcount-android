6d51b723238e6ffa6d4b226ae523feced8ba0184
Shawn <paramountstarllc@gmail.com>
2018-09-12 08:38:41 +0800

    - fixec crash issue on file share #8

--- app/src/main/java/com/socketmobile/stockcount/ui/EditActivity.kt
+++ app/src/main/java/com/socketmobile/stockcount/ui/EditActivity.kt
@@ -12,6 +12,7 @@ import android.os.VibrationEffect
 import android.os.Vibrator
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
+import android.support.v4.content.FileProvider
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.text.method.TextKeyListener
@@ -194,7 +195,8 @@ class EditActivity : AppCompatActivity() {
 
         val i = Intent(Intent.ACTION_SEND)
         i.type = "text/plain"
-        i.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(tempFile))
+        i.putExtra(Intent.EXTRA_STREAM, FileProvider.getUriForFile(this, applicationContext.packageName + ".fileProvider", tempFile))
+        i.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
         val chooser = Intent.createChooser(i, "Share via")
         startActivity(chooser)
     }
