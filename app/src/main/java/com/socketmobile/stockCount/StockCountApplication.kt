a867191cccbdb34271e7ae663276544b56b07e82
Frank <luck_bf@outlook.com>
2019-02-02 20:37:46 +0800

    - added auto start service code

--- app/src/main/java/com/socketmobile/stockCount/StockCountApplication.kt
+++ app/src/main/java/com/socketmobile/stockCount/StockCountApplication.kt
@@ -3,6 +3,8 @@
 package com.socketmobile.stockCount
 
 import android.app.Application
+import android.content.ComponentName
+import android.content.Intent
 import android.os.Environment
 import com.crashlytics.android.Crashlytics
 import com.socketmobile.capture.android.Capture
@@ -15,6 +17,9 @@ import java.io.IOException
 
 
 class StockCountApplication: Application() {
+    val captureServicePackage = "com.socketmobile.companion"
+    val captureServiceStartCmd = "com.socketmobile.capture.StartService"
+
     override fun onCreate() {
         super.onCreate()
         Realm.init(applicationContext)
@@ -42,11 +47,20 @@ class StockCountApplication: Application() {
                 e.printStackTrace()
             }
         }
+
+        startCaptureService()
     }
 
 
     private fun isExternalStorageWritable(): Boolean {
         val state = Environment.getExternalStorageState()
         return Environment.MEDIA_MOUNTED == state
+    }
+
+    private fun startCaptureService() {
+        val intent = Intent("com.socketmobile.capture.START_SERVICE")
+        intent.component = ComponentName(captureServicePackage, captureServiceStartCmd)
+        intent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND)
+        sendBroadcast(intent)
     }
 }
