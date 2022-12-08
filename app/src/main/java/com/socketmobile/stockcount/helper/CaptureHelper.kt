c4e2ea97b04a98427a9fbd16048de34f1d5571ed
Shawn Dhanani <paramountstarllc@gmail.com>
2022-12-08 09:46:54 -0500

    - add SocketCam support

--- app/src/main/java/com/socketmobile/stockcount/helper/CaptureHelper.kt
+++ app/src/main/java/com/socketmobile/stockcount/helper/CaptureHelper.kt
@@ -1,4 +1,6 @@
 /**  Copyright © 2018 Socket Mobile, Inc. */
 
 package com.socketmobile.stockcount.helper
-
+fun isSocketCamDevice(deviceName: String): Boolean {
+    return deviceName == "SocketCam C820" || deviceName == "SocketCam C860"
+}
